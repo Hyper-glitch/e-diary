0467dbbab50a478ce7837aaab0b60536833d63f9
Roman <graff049@gmail.com>
2022-06-26 23:29:01 +0300

    feature:(compliments) add more compliments docs: add doc string for create_commendation feature:(compliments) get random compliments

--- datacenter/hack_db_tools.py
+++ datacenter/hack_db_tools.py
@@ -1,7 +1,20 @@
+"""Module helps to interact with e-diary database and change there data"""
 import random
+
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
 from datacenter.models import Mark, Chastisement, Lesson, Commendation, Schoolkid
+
+COMPLIMENTS = [
+    'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
+    'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
+    'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
+    'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!',
+    'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!',
+    'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!',
+    'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!',
+    'Теперь у тебя точно все получится!',
+]
 
 
 def fix_marks(schoolkid):
@@ -10,12 +23,13 @@ def fix_marks(schoolkid):
 
 
 def remove_chastisements(schoolkid):
-    """Filter chastisement for current schoolkid and delete all of them."""
+    """Filter chastisement for current schoolkid and delete all."""
     chastisement = Chastisement.objects.filter(schoolkid=schoolkid.first())
     chastisement.delete()
 
 
-def create_commendation(subject, text, schoolkid_name, teacher):
+def create_commendation(subject, schoolkid_name, teacher):
+    """Create commendation for given subject and schoolkid."""
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
     except MultipleObjectsReturned:
@@ -26,6 +40,6 @@ def create_commendation(subject, text, schoolkid_name, teacher):
         lessons = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title=subject.first().title)
         lesson = random.choice(lessons)
         Commendation.objects.create(
-            text=text, created=lesson.date, schoolkid=schoolkid.first(),
+            text=random.choice(COMPLIMENTS), created=lesson.date, schoolkid=schoolkid.first(),
             subject=subject.first(), teacher=teacher.first(),
         )
